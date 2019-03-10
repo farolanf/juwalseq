d2985f0ac07c786779c4d7cf555c189f3e5d8ad4
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-10 17:52:25 +0800

    remove debug component

--- frontend/src/components/pages/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { observer, inject } from 'mobx-react'
-
-const Test = ({ store }) => (
-  <div>Hello {store.user.user}</div>
-)
-
-export default inject('store')(observer(Test))
