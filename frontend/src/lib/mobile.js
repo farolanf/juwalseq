d89b9db59aed7a89304b6663681a6d1d64d92103
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-14 10:07:53 +0800

    add mobile helper

---
+++ frontend/src/lib/mobile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import withWidth, { isWidthDown } from '@material-ui/core/withWidth'
+
+const withMobile = component => 
+  withWidth()(props => {
+    const mobile = isWidthDown('sm', props.width)
+    return React.createElement(component, { ...props, mobile })
+  })
+
+export default withMobile
