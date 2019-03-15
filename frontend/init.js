d0461c525aee61b100db058769d9874c8c2f0286
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-15 15:23:06 +0800

    add validation messages

--- frontend/init.js
+++ frontend/init.js
@@ -1,5 +1,13 @@
+import { setLocale } from 'yup'
 import { configure } from 'mobx'
 import { verify } from '$src/lib/auth'
+import messages from '$prj/locale/validation'
+
+// yup doesn't support unknown messages
+const _messages = Object.assign({}, messages)
+delete _messages.test
+
+setLocale(_messages)
 
 configure({
   enforceActions: 'always',
---
+++ frontend/locale/validation.js
@@ -0,0 +1,15 @@
+const messages = {
+  mixed: {
+    required: 'required',
+  },
+  string: {
+    min: 'must be at least ${min} characters',
+    max: 'must be at most ${max} characters',
+  },
+  test: {
+    uniqueEmail: 'email is taken',
+    passwordConfirm: 'both passwords must be equal',
+  }
+}
+
+export default messages
