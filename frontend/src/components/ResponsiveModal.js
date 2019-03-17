3104fd47cd3364fa41e8c021fa63390f6b593ef5
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-17 14:32:45 +0800

    add ResponsiveModal states

--- frontend/src/components/ResponsiveModal.js
+++ frontend/src/components/ResponsiveModal.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import cn from 'classnames'
+import withMobile from '$lib/mobile'
 
-const ResponsiveModal = ({ mobile, dialogClass, children, ...props }) => {
+const ResponsiveModal = ({ mobile, width, dialogClass, children, modalRef, ...props }) => {
   const [ref, setRef] = useState()
   const [closeRef, setCloseRef] = useState()
-
-  console.log('mobile', mobile)
+  const [show, setShow] = useState()
+  const [shown, setShown] = useState()
 
   useEffect(() => {
     UIkit.util.toggleClass(ref, 'uk-modal-full', mobile)
@@ -13,14 +14,31 @@ const ResponsiveModal = ({ mobile, dialogClass, children, ...props }) => {
     UIkit.util.toggleClass(closeRef, 'uk-modal-close-default', !mobile)
   })
 
+  useEffect(() => {
+    if (ref) {
+      const offs = [
+        UIkit.util.on(ref, 'show', () => setShow(true)),
+        UIkit.util.on(ref, 'hide', () => setShow(false)),
+        UIkit.util.on(ref, 'shown', () => setShown(true)),
+        UIkit.util.on(ref, 'hidden', () => setShown(false)),
+      ]
+      return () => offs.forEach(off => off())
+    }
+  }, [ref])
+
+  function _setRef (ref) {
+    setRef(ref)
+    modalRef && modalRef(ref)
+  }
+
   return (
-    <div data-uk-modal ref={setRef} {...props}>
+    <div data-uk-modal ref={_setRef} {...props}>
       <div className={cn('uk-modal-dialog uk-modal-body', dialogClass)} >
         <button className='uk-modal-close-default' type='button' data-uk-close ref={setCloseRef} />
-        {children}
+        {typeof children === 'function' ? children({ show, shown }) : children}
       </div>
     </div>
   )
 }
 
-export default ResponsiveModal
+export default withMobile(ResponsiveModal)
