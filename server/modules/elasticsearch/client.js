db604e2a364b66cb0513ffd52888e70513194a83
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-23 22:27:34 +0800

    add elastic search env

--- server/modules/elasticsearch/client.js
+++ server/modules/elasticsearch/client.js
@@ -1,8 +1,16 @@
 const elasticsearch = require('elasticsearch')
 
-const client = elasticsearch.Client({
-  apiVersion: '6.6'
-})
+const options = {
+  apiVersion: '6.6',
+  host: process.env.ELASTICSEARCH_HOST || 'localhost:9200',
+  maxSockets: process.env.ELASTICSEARCH_MAX_SOCKETS
+}
+
+if (process.env.ELASTICSEARCH_USER && process.env.ELASTICSEARCH_PASS) {
+  options.httpAuth = `${process.env.ELASTICSEARCH_USER}:${process.env.ELASTICSEARCH_PASS}`
+}
+
+const client = elasticsearch.Client(options)
 
 client.ping({
   requestTimeout: 5000
