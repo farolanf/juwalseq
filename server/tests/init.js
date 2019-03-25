ce1f136d1646674b7f28a031fc10bd4d4f9f3f14
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-25 08:23:35 +0800

    init test db

--- server/tests/init.js
+++ server/tests/init.js
@@ -3,3 +3,9 @@ global.assert = global.chai.assert
 
 global.chai.use(require('chai-http'))
 global.request = global.chai.request
+
+const { initdb } = require('@test/helpers/db')
+
+before(() => {
+  return initdb()
+})
