e999c7947776fba6d26127742185c77cf7b51966
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-27 10:11:04 +0800

    set title on SSR

--- frontend/gatsby-ssr.js
+++ frontend/gatsby-ssr.js
@@ -1,10 +1,24 @@
+import React from 'react'
+
 import './init'
 import _wrapRootElement from './wrapRootElement'
 
+import config from '$prj/gatsby-config'
+
+// we use wrapPageElement instead of wrapRootElement so we can test if
+// the page is an Admin page to avoid wrapping it
 export const wrapPageElement = ({ element }) => {
   // no ssr for admin
   if (element.props['*'].startsWith('admin')) {
     return null
   }
   return _wrapRootElement(element)
+}
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const components = getHeadComponents()
+  components.unshift(
+    <title key='siteTitle'>{config.siteMetadata.title}</title>
+  )
+  replaceHeadComponents(components)
 }
--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+
+import SEO from '$comp/SEO'
 import Header from './Header'
 
 const PageLayout = ({ children }) => (
@@ -17,6 +19,7 @@ const PageLayout = ({ children }) => (
     `}
     render={data => (
       <div className='uk-container px-0'>
+        <SEO title={data.site.siteMetadata.title} />
         <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
       </div>
