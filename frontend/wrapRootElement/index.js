bf3da71ff9c36dca45f7ed3ce474f9bb3be66583
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-30 09:48:50 +0800

    fix tailwind slow load

--- frontend/wrapRootElement/index.js
+++ frontend/wrapRootElement/index.js
@@ -1,7 +1,7 @@
 import 'uikit/dist/css/uikit.min.css'
-import '$assets/css/alegreya.css'
 import '$assets/css/tailwind.css'
 import '$assets/css/global.css'
+import '$assets/css/alegreya.css'
 
 const wrapRootElement = element => element
 
