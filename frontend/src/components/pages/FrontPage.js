2a7d950964b75b3e6058cc75c86061a473c8feb1
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-31 22:10:27 +0800

    update button style

--- frontend/src/components/pages/FrontPage.js
+++ frontend/src/components/pages/FrontPage.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const FrontPage = () => (
   <div>
-    <h1 className='p-8'>Welcome to Juwal!</h1>
-    <button className='btn btn-blue'>Click</button>
-    <button className='btn bg-grey-darker dark'>Click</button>
+    <h1>Welcome to Juwal!</h1>
+    <button className='btn'>Default</button>
+    <button className='btn btn-primary'>Default</button>
+    <button className='btn btn-secondary'>Default</button>
+    <div className='bg-grey-dark p-8 mt-4'>
+      <button className='btn btn-dark'>Dark</button>
+      <button className='btn btn-primary'>Default</button>
+      <button className='btn btn-secondary'>Default</button>
+    </div>
   </div>
 )
 
