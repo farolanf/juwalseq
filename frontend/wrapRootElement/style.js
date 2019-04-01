1331bfd2d993e132d1eeef32c5fdd470a68dd835
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-01 21:19:49 +0800

    fix style order

--- frontend/wrapRootElement/style.js
+++ frontend/wrapRootElement/style.js
@@ -1,7 +1,7 @@
 import 'font-awesome/css/font-awesome.min.css'
-import '$assets/css/tailwind.css'
 import '$assets/css/theme/theme.css'
 import '$assets/css/global.css'
+import '$assets/css/tailwind.css'
 
 const wrapRootElement = element => element
 
