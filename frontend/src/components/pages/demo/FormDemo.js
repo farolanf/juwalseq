7dfd1088617c39a2ee70b485263eba72fef63fa9
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-05 08:44:33 +0800

    add dark popup demo

--- frontend/src/components/pages/demo/FormDemo.js
+++ frontend/src/components/pages/demo/FormDemo.js
@@ -79,18 +79,30 @@ const FormDemo = () => (
       <button className='btn btn-primary' disabled>Disabled</button>
       <button className='btn btn-secondary' disabled>Secondary</button>
     </div>
-    <h2>Popup</h2>
+    <h2 className='mt-4'>Popup</h2>
+    <h4>Light</h4>
     <div className='list-x-1'>
       <button className='btn btn-primary' id='popup-top'>Top</button>
-      <button className='btn btn-primary' id='popup-right'>Right</button>
       <button className='btn btn-primary' id='popup-bottom'>Bottom</button>
       <button className='btn btn-primary' id='popup-left'>Left</button>
+      <button className='btn btn-primary' id='popup-right'>Right</button>
       <Popup target='#popup-top' pos='top' hover>Top</Popup>
       <Popup target='#popup-right' pos='right' hover>Right</Popup>
       <Popup target='#popup-bottom' pos='bottom' hover>Bottom</Popup>
       <Popup target='#popup-left' pos='left' hover>Left</Popup>
     </div>
-    <h2>Alert</h2>
+    <h4 className='mt-4'>Dark</h4>
+    <div className='list-x-1'>
+      <button className='btn btn-primary' id='popup-top2'>Top</button>
+      <button className='btn btn-primary' id='popup-bottom2'>Bottom</button>
+      <button className='btn btn-primary' id='popup-left2'>Left</button>
+      <button className='btn btn-primary' id='popup-right2'>Right</button>
+      <Popup target='#popup-top2' pos='top' hover className='popup-dark'>Top</Popup>
+      <Popup target='#popup-right2' pos='right' hover className='popup-dark'>Right</Popup>
+      <Popup target='#popup-bottom2' pos='bottom' hover className='popup-dark'>Bottom</Popup>
+      <Popup target='#popup-left2' pos='left' hover className='popup-dark'>Left</Popup>
+    </div>
+    <h2 className='mt-4'>Alert</h2>
     <h4>Info</h4>
     <div className='alert'>
       <span className='close' />
