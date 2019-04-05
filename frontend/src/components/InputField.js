b52112c6ed884d1fa08bea3655ce304b0d9fffe2
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-05 08:02:35 +0800

    update InputField

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Field, connect } from 'formik'
 
-const InputField = ({ name, icon, className, extra, formik: { touched, errors }, ...props }) => (
-  <div className={cn('form-field', className)}>
-    <div className='form-control'>
-      {icon && <span className='input-prefix pl-3'><i className={'fa fa-' + icon} /></span>}
-      <Field key={name} className={cn('input', icon && 'pl-8', touched[name] && errors[name] && 'has-error')} name={name} {...props} />
-    </div>
-    {touched[name] && errors[name] && (
-      <div className='field-error'>
-        {_.upperFirst(errors[name])}
+const InputField = ({ name, label, icon, leftPrefix, className, inputClass, help, extra, full, inputRef, formik: { touched, errors }, ...props }) => (
+    <div className={cn('form-field', className)}>
+      {label && <label className='field-label'>{label}</label>}
+      <div className={cn('field-control', full && 'field-control-full')}>
+        <div className='form-control'>
+          {leftPrefix && <span className='input-prefix pl-3'>{leftPrefix}</span>}
+          {icon && <span className='input-prefix pl-3'><i className={'fa fa-' + icon} /></span>}
+          <Field key={name} className={cn('input max-h-xs', icon && 'pl-8', touched[name] && errors[name] && 'has-error', inputClass)} name={name} {...props} innerRef={inputRef} />
+        </div>
+        {touched[name] && errors[name] && (
+          <div className='field-error'>
+            {_.upperFirst(errors[name])}
+          </div>
+        )}
+        {help ? (
+          <div className='flex justify-between'>
+            <div className='field-help'>{help}</div>
+            {extra}
+          </div>
+        ) : extra}
       </div>
-    )}
-    {extra}
-  </div>
-)
+    </div>
+  )
 
 export default connect(InputField)
