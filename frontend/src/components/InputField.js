246bbfd898cf6842e11fb2da1acbaa9ee30a0715
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-09 14:08:28 +0800

    support select

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Field, connect } from 'formik'
 
-const InputField = ({ name, label, icon, leftPrefix, className, inputClass, help, extra, full, inputRef, formik: { touched, errors }, ...props }) => (
+const componentClass = {
+  input: 'input',
+  textarea: 'input',
+  select: 'select',
+}
+
+const InputField = ({ name, label, icon, leftPrefix, className, inputClass, help, extra, full, inputRef, component = 'input', formik: { touched, errors }, children, ...props }) => (
     <div className={cn('form-field', className)}>
       {label && <label className='field-label'>{label}</label>}
       <div className={cn('field-control', full && 'field-control-full')}>
         <div className='form-control'>
           {leftPrefix && <span className='input-prefix pl-3'>{leftPrefix}</span>}
           {icon && <span className='input-prefix pl-3'><i className={'fa fa-' + icon} /></span>}
-          <Field key={name} className={cn('input max-h-xs', icon && 'pl-8', touched[name] && errors[name] && 'has-error', inputClass)} name={name} {...props} innerRef={inputRef} />
+          <Field key={name} className={cn('max-h-xs', componentClass[component], icon && 'pl-8', touched[name] && errors[name] && 'has-error', inputClass)} name={name} component={component} {...props} innerRef={inputRef}>
+            {children}
+          </Field>
         </div>
         {touched[name] && errors[name] && (
           <div className='field-error'>
