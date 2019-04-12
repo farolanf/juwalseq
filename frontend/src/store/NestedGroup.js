947e9c3f40ff92e719aa6a03485fcbe587ba3cee
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-12 21:53:45 +0800

    fix children not assigned to parent

--- frontend/src/store/NestedGroup.js
+++ frontend/src/store/NestedGroup.js
@@ -1,8 +1,10 @@
 import { observable, flow, computed } from 'mobx'
+import pluralize from 'pluralize'
 
 class NestedGroup {
   @observable parents = {}
   parentsLoaded = false
+  parentsLoading = false
 
   constructor (fetchParentsApi, fetchChildrenApi, parentName, childrenName) {
     this.parentName = parentName
@@ -21,6 +23,9 @@ class NestedGroup {
         return this.parents
       }
     })
+    this[`get${pluralize.singular(childrenName)}`] = (parentId, childId) => {
+      return this.parents[parentId] && this.parents[parentId].children && this.parents[parentId].children.find(child => child.id == childId)
+    }
   }
 
   @computed
@@ -32,7 +37,8 @@ class NestedGroup {
   }
 
   fetchParents = flow(function* () {
-    if (this.parentsLoaded) return
+    if (this.parentsLoaded || this.parentsLoading) return
+    this.parentsLoading = true
     const parents = yield this.fetchParentsApi().then(res => res.data)
     _.each(parents, parent => {
       if (this.parents[parent.id]) {
@@ -44,25 +50,24 @@ class NestedGroup {
         this.parents[parent.id] = parent
       }
     })
-    this.parents = _.clone(this.parents)
     this.parentsLoaded = true
+    this.parentsLoading = false
   })
 
   fetchChildren = flow(function* (parentId) {
     if (!parentId) throw new Error('Missing parentId')
-    const parent = this.parents[parentId] || {}
-    if (parent.children) return
-    if (parent.childrenLoading) return
-    parent.childrenLoading = true
-    this.parents[parentId] = parent
-    parent.children = yield this.fetchChildrenApi(parentId).then(res => res.data)
-    Object.defineProperty(parent, _.camelCase(this.childrenName), {
+    if (!this.parents[parentId]) {
+      this.parents[parentId] = {}
+    }
+    if (this.parents[parentId].children || this.parents[parentId].childrenLoading) return
+    this.parents[parentId].childrenLoading = true
+    this.parents[parentId].children = yield this.fetchChildrenApi(parentId).then(res => res.data)
+    Object.defineProperty(this.parents[parentId], _.camelCase(this.childrenName), {
       get () {
         return this.children
       }
     })
-    this.parents = _.clone(this.parents)
-    delete parent.childrenLoading
+    this.parents[parentId].childrenLoading = false
   })
 }
 
