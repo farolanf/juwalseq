83d009118a061c1f188473b6d9010f91de96c15a
Farolan Faisal <farolanfaisal@gmail.com>
2019-04-14 07:38:25 +0800

    add FormFIeld label

--- frontend/src/components/FormField.js
+++ frontend/src/components/FormField.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const FormField = ({ full, className, containerClass, children }) => (
+const FormField = ({ label, full, className, containerClass, children }) => (
   <div className={cn('form-field', className)}>
-    <label className='field-label hidden md:block' />
+    <label className='field-label hidden md:block'>{label}</label>
     <div className={cn('field-control', full && 'field-control-full')}>
       <div className={cn('form-control', containerClass)}>{children}</div>
     </div>
